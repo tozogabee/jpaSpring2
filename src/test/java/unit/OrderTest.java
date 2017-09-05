0358c44dafb26b032a37b49f2df33635ec2002d2
tozogabee <tozogabee@gmail.com>
2017-09-05 20:33:53 +0200

    Order test created

---
+++ src/test/java/unit/OrderTest.java
@@ -0,0 +1,19 @@
+package unit;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ * Created by java on 2017.09.05..
+ */
+public class OrderTest {
+
+
+
+    @BeforeClass
+    public void setUp()
+    {
+
+    }
+
+}
